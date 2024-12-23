c29644beae6c3cfb9ae378366127d2df5f468e9d
itachi sasuke <8012032+Keithwachira@users.noreply.github.com>
2024-12-23 08:38:23 +0300

    add data

--- scripts/modify_version_json.py
+++ scripts/modify_version_json.py
@@ -22,13 +22,14 @@ def update_version_file(current_release, next_release):
     versions_data.insert(0, new_version)
     previous_latest_index = next((i for i, version in enumerate(versions_data) if version['branch'] == current_release),
                                  None)
-    version_old_latest_number = current_release.replace("release-", "")
-    old_latest_version = {
-        "path": f"/docs/{version_old_latest_number}/",
-        "name": version_old_latest_number,
-        "branch": current_release
-    }
-    versions_data[previous_latest_index] = old_latest_version
+    if previous_latest_index is not None:
+        version_old_latest_number = current_release.replace("release-", "")
+        old_latest_version = {
+            "path": f"/docs/{version_old_latest_number}/",
+            "name": version_old_latest_number,
+            "branch": current_release
+        }
+        versions_data[previous_latest_index] = old_latest_version
     with open(file_path, 'w') as file:
         json.dump(versions_data, file, indent=4)
         print("Modified versions data:")
